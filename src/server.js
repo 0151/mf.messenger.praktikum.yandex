03d1b74c5a492ae970e76d72c6b5f872fe88b71f
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2020-06-12 18:35:01 +0300

    Первый спринт

---
+++ src/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const port = process.env.PORT || 3000
+
+const express = require('express')
+const app = express()
+
+app.use(express.static('./static'))
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
