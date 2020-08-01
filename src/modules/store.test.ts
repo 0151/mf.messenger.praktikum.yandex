ca565a8c64d98085ee532a9d5a4f69edd8c8b189
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2020-08-01 11:43:29 +0300

    Добавляет простую реализацию Flux

---
+++ src/modules/store.test.ts
@@ -0,0 +1,29 @@
+import { Store, combineReducers } from './store'
+
+describe('Стор', () => {
+
+    test('работает подписка на изменения стора', () => {
+        const cb = jest.fn()
+
+        const auth = (state = false, action) => {
+            switch (action.type) {
+            case 'SIGNIN': return true
+            case 'SIGNOUT': return false
+            default: return state
+            }
+        }
+
+        const rootReducer = combineReducers({
+            auth
+        })
+
+        const store = new Store(rootReducer)
+
+        store.subscribe(cb)
+
+        store.dispatch({ type: 'TEST' })
+
+        expect(cb).toBeCalled()
+    })
+
+})
--- src/modules/store.ts
+++ src/modules/store.ts
@@ -1,31 +1,51 @@
-interface Action {
+interface IAction {
     type: string
-    payload?: unknown
+    payload?: Record<string, unknown>
 }
 
-const action: Action = {
-    type: 'ADD_TODO',
-    payload: { label: 'Съесть пиццу,', complete: false },
-  };
+type State = unknown
+
+type Reducer = (state: State, action: IAction) => void
 
 export class Store {
-    private state: Record<string, unknown>
-  
-    constructor() {
-      this.state = {}
-    }
-  
-    get value() {
-      return this.state
-    }
-  
-    dispatch(action: Action) {}
-  
-    subscribe() {}
-  }
+    private _currentReducer: Reducer
+    private _currentState: State
+    private _currentListeners: (() => void)[]
 
-const sampleReducer = () => {
-    
+    constructor(reducer: Reducer, preloadedState = {}) {
+        this._currentReducer = reducer
+        this._currentState = preloadedState
+        this._currentListeners = []
+    }
+
+    get getState(): State {
+        return this._currentState
+    }
+
+    subscribe(listener: () => void): void {
+        this._currentListeners.push(listener)
+    }
+
+    dispatch(action: IAction): void {
+        this._currentState = this._currentReducer(this._currentState, action)
+
+        this._currentListeners.forEach(listener => listener())
+    }
 }
 
-export const store = new Store()
+export const combineReducers = (reducers: Record<string, Reducer>) => {
+
+    return function(state = {}, action: IAction): State {
+        const nextState = {}
+
+        for (const key in reducers) {
+            const reducer = reducers[key]
+            const previousStateForKey = state[key]
+            const nextStateForKey = reducer(previousStateForKey, action)
+
+            nextState[key] = nextStateForKey
+        }
+
+        return nextState
+    }
+}
