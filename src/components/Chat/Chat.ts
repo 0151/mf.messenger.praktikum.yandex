c782ae0a544f205b16a854e8882b0dcfd7cd9106
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2020-08-06 18:12:21 +0300

    Добавляет компонент чата

---
+++ src/components/Chat/Chat.ts
@@ -0,0 +1,13 @@
+import { Component } from '../../modules/component'
+import template from './Chat.handlebars'
+import './Chat.scss'
+
+export class Chat extends Component {
+    componentDidMount() {
+        //
+    }
+
+    render() {
+        return template
+    }
+}
